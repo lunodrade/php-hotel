3e653653bb63e8204c08c1314fd97f4e03a9b7fd
Luciano Andrade <lunodrade@gmail.com>
2014-11-15 00:34:21 -0200

    Configurando servidor
    Modificou a constante para aceitar o servidor

--- constantes.php
+++ constantes.php
@@ -4,9 +4,9 @@
 	    session_start();
 	}
 	
-	if (!defined('URL')) define('URL', 'http://127.0.0.1/projects/projectCake');
+	if (!defined('URL')) define('URL', 'http://labs.lucianoandrade.me/hotel');
 	if (!defined('DS'))  define('DS', '/');
 
-	if (!defined('DEBUG'))  define('DEBUG', TRUE);
+	if (!defined('DEBUG'))  define('DEBUG', FALSE);
 
 ?>
