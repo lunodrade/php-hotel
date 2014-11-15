d12b6531569a95a5bcdc4f36d13eb7dc010cf9cf
Luciano Andrade <lunodrade@gmail.com>
2014-11-15 05:23:06 -0200

    Auth (admin or user)

--- auth/interno_usuario.php
+++ auth/interno_usuario.php
@@ -1,19 +1,4 @@
 <?php 
-//require_once 'usuario.php';
-//require_once 'sessao.php';
-//require_once 'autenticador.php';
-//
-//
-//$aut = Autenticador::instanciar();
-//
-//$usuario = null;
-//if ($aut->esta_logado()) {
-//    $usuario = $aut->pegar_usuario();
-//}
-//else {
-//    $aut->expulsar();
-//}
-
 require_once 'perm_user.php';
 ?>
 
