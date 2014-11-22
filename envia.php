7e8263dc4110fc2d20b9ddec0826c01384d41ef1
Luciano Andrade <lunodrade@gmail.com>
2014-11-22 16:56:08 -0200

    up

--- envia.php
+++ envia.php
@@ -1,29 +1,35 @@
 <?php
-// Passando os dados obtidos pelo formulário para as variáveis abaixo
-$assunto          = $_POST['assunto'];
-$nomeremetente = $_POST['nome'];
-$emailremetente    = trim($_POST['e-mail']);
-$mensagem          = $_POST['mensagem'];
-$mensagem = nl2br($mensagem); // Para manter os parágrafos e quebras de linha
-$emaildestinatario = [EMAIL="'hotel@lucianoandrade.me'"]'hotel@lucianoandrade.me'[/EMAIL]; // Seu e-mail aqui, lembrando que o e-mail deve estar em seu servidor web
- 
-/* Montando a mensagem a ser enviada no corpo do e-mail. */
-$mensagemHTML = '<P>FORMULARIO PREENCHIDO NO SITE [URL="http://www.meudominio.com.br</P"]www.meudominio.com.br</P[/URL]>
-<p><b>Nome:</b> '.$nomeremetente.'
-<p><b>E-Mail:</b> '.$emailremetente.'
-<p><b>Assunto:</b> '.$assunto.'
-<p><b>Mensagem:</b> '.$mensagem.'</p>
-<hr>';
-// O remetente deve ser um e-mail do seu domínio conforme determina a RFC 822.
-// O return-path deve ser ser o mesmo e-mail do remetente.
-$headers = "MIME-Version: 1.1\r\n";
-$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
-$headers .= "From: $emailremetente\r\n"; // remetente
-$headers .= "Return-Path: $emaildestinatario \r\n"; // return-path
- 
-$envio = mail($emaildestinatario, $assunto, $mensagemHTML, $headers); 
- 
- if($envio)
-//echo "<script>location.href='sucesso.html'</script>"; // Página que será redirecionada
-echo "enviado!"; // Página que será redirecionada
+$to = "uchiha.luciano@gmail.com";
+$subject = "HTML email";
+
+$message = "
+<html>
+<head>
+<title>HTML email</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>
+<tr>
+<td>John</td>
+<td>Doe</td>
+</tr>
+</table>
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <hotel@lucianoandrade.me>' . "\r\n";
+$headers .= 'Cc: andrade.luciano@live.com' . "\r\n";
+
+mail($to,$subject,$message,$headers);
 ?>
